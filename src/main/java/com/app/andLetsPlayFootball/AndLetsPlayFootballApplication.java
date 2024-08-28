065168f6c180ed6147881a766e80d982519b3d4c
michelecossuu <michelecossu01@gmail.com>
2024-08-28 15:39:05 +0200

    Add project

---
+++ src/main/java/com/app/andLetsPlayFootball/AndLetsPlayFootballApplication.java
@@ -0,0 +1,13 @@
+package com.app.andLetsPlayFootball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndLetsPlayFootballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndLetsPlayFootballApplication.class, args);
+	}
+
+}
