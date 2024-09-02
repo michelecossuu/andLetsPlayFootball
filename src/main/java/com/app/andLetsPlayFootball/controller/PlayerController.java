a04790d72fc8debb58a1e4669a36c0b6f27cece9
michelecossuu <michelecossu01@gmail.com>
2024-09-02 13:13:46 +0200

    Add Player Controller

---
+++ src/main/java/com/app/andLetsPlayFootball/controller/PlayerController.java
@@ -0,0 +1,35 @@
+package com.app.andLetsPlayFootball.controller;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.app.andLetsPlayFootball.model.Player;
+import com.app.andLetsPlayFootball.service.PlayerService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/api/players")
+public class PlayerController {
+
+    Logger logger = Logger.getLogger(PlayerController.class.getName());
+    
+    private final PlayerService playerService;
+
+    public PlayerController(PlayerService playerService) {
+        this.playerService = playerService;
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Player>> searchPlayers(@RequestParam("nome") String name) {
+        List<Player> players = playerService.searchPlayers(name);
+        return ResponseEntity.ok(players);
+    }
+    
+
+}
