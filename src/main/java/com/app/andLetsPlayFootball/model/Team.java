3bfcb900b569a6d1485552a8e394b1bf8f97c661
michelecossuu <michelecossu01@gmail.com>
2024-09-02 17:42:10 +0200

    Add team entity

---
+++ src/main/java/com/app/andLetsPlayFootball/model/Team.java
@@ -0,0 +1,36 @@
+package com.app.andLetsPlayFootball.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "teams")
+public class Team {
+    
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Team [id=" + id + ", name=" + name + "]";
+    }
+
+}
+
