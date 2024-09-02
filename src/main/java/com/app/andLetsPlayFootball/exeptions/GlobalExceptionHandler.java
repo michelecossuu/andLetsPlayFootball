df0561cb127cc941e5d2784e34e87fcd6f6a9476
michelecossuu <michelecossu01@gmail.com>
2024-09-02 13:13:56 +0200

    Add a global exception handler

---
+++ src/main/java/com/app/andLetsPlayFootball/exeptions/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.app.andLetsPlayFootball.exeptions;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        return null;
+        // Logica per gestire le eccezioni
+    }
+}
