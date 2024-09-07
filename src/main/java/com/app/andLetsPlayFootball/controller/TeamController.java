6c229480ada546952c9db990f53c4c68e21f34b6
michelecossuu <michelecossu01@gmail.com>
2024-09-07 10:41:58 +0200

    Add DTO mapping

--- src/main/java/com/app/andLetsPlayFootball/controller/TeamController.java
+++ src/main/java/com/app/andLetsPlayFootball/controller/TeamController.java
@@ -1,5 +1,6 @@
 package com.app.andLetsPlayFootball.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.app.andLetsPlayFootball.dto.TeamDTO;
+import com.app.andLetsPlayFootball.mapper.PlayerMapper;
 import com.app.andLetsPlayFootball.model.Team;
 import com.app.andLetsPlayFootball.service.TeamService;
 
@@ -19,15 +22,21 @@ public class TeamController {
     Logger logger = Logger.getLogger(TeamController.class.getName());
 
     private final TeamService teamService;
+    private final PlayerMapper mapper;
 
-    public TeamController(TeamService teamService) {
+    public TeamController(TeamService teamService, PlayerMapper mapper) {
         this.teamService = teamService;
+        this.mapper = mapper;
     }
 
     @GetMapping("/search")
-    public ResponseEntity<List<Team>> searchTeams(@RequestParam("name") String name) {
+    public ResponseEntity<List<TeamDTO>> searchTeams(@RequestParam("name") String name) {
         List<Team> teams = teamService.searchTeams(name);
-        return ResponseEntity.ok(teams);
+        List<TeamDTO> teamsDTO = new ArrayList<>();
+        for (Team t : teams) {
+            teamsDTO.add(mapper.teamToTeamDTO(t));
+        }
+        return ResponseEntity.ok(teamsDTO);
     }
     
 }
